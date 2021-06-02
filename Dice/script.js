25c7a783625c8654c6e3bb97a754f6164e7b64f8
Naveen Basireddy <54843177+naveenbasireddy@users.noreply.github.com>
2021-06-03 01:28:20 +0530

    Add files via upload

---
+++ Dice/script.js
@@ -0,0 +1,31 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSouce = "./images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute('src', randomImageSouce);
+
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSouce2 = "./images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute('src', randomImageSouce2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = "🥇Player 1 Wins!"
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector('h1').innerHTML = "Player 2 Wins!🥇"
+}
+else  {
+   document.querySelector('h1').innerHTML = "It's a Draw"
+}
