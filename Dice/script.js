a7e6d9c3e4d94c353a7dcedc4034d261b3c73a1a
Naveen Basireddy <54843177+naveenbasireddy@users.noreply.github.com>
2021-06-03 20:35:33 +0530

    Update script.js

--- Dice/script.js
+++ Dice/script.js
@@ -1,37 +1,31 @@
 if (window.performance.navigation.type === 1) {
+   // if the page has been refreshed, call the rollDice() function.
   rollDice();
 }
+The rollDice() function just does the rest of the javascript.
+
+My entire solution:
+
+if (window.performance.navigation.type === 1) {
+  rollDice();
+}
+ 
 function rollDice() {
-
-var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-
-var randomImageSouce = "./images/" + randomDiceImage;
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute('src', randomImageSouce);
-
-
-var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-
-var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-
-var randomImageSouce2 = "./images/" + randomDiceImage2;
-
-var image2 = document.querySelectorAll("img")[1];
-
-image2.setAttribute('src', randomImageSouce2);
-
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector('h1').innerHTML = "🥇Player 1 Wins!"
+  var randomNumber1 = `images/dice${Math.floor(Math.random() * 6) + 1}.png`;
+  var randomNumber2 = `images/dice${Math.floor(Math.random() * 6) + 1}.png`;
+ 
+  document.querySelector(".img1").setAttribute("src", randomNumber1);
+  document.querySelector(".img2").setAttribute("src", randomNumber2);
+ 
+  changeTitle(randomNumber1, randomNumber2);
 }
-else if (randomNumber1 < randomNumber2) {
-  document.querySelector('h1').innerHTML = "Player 2 Wins!🥇"
-}
-else  {
-   document.querySelector('h1').innerHTML = "It's a Draw"
-}
+ 
+function changeTitle(num1, num2) {
+  if (num1 === num2) {
+    document.querySelector("h1").innerHTML = "It's a Draw!";
+  } else if (num1 > num2) {
+    document.querySelector("h1").innerHTML = "🥇Player 1 Wins!";
+  } else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins🥇";
+  }
 }
